e41359c3d7dd1da9701082f88aa68ef57298fa25
KamranMahdavi <Kamranz.mahdaviz@gmail.com>
2026-07-09 01:59:12 -0300

    Implement binarization and update helper variable generation

--- converter.py
+++ converter.py
@@ -1,11 +1,11 @@
 def normalize_start_symbol(formalized_grammar):
     products = formalized_grammar["productions"]
+    variables = formalized_grammar["variables"]
     if not _check_start_RHS(products):
-        new_variable = _generate_new_variable(products, start=True)
+        new_variable = _generate_new_variable(products, variables, start=True)
         formalized_grammar["start"] = new_variable
 
-def _generate_new_variable(productions, start=False):
-    variables = set(productions.keys())
+def _generate_new_variable(productions, variables, rules=None, start=False):
     index = 0
     new_variable = "S" if start == True else "A"
 
@@ -15,15 +15,17 @@ def _generate_new_variable(productions, start=False):
     new_variable += f'{index}'
 
     if start == True:
-        productions[new_variable] = ["S"]
-        return new_variable
+        productions[new_variable] = {("S",)}    
+    else:
+        productions[new_variable] = {rules}
+
+    variables.add(new_variable)
+    return new_variable
 
 def _check_start_RHS(productions):
     start_RHS = productions['S']
-    symbols = set()
     for element in start_RHS:
-        symbols = set(element.split(" "))
-        if "S" in symbols:
+        if "S" in element:
             return False
     return True
 
@@ -106,3 +108,31 @@ def _remove_unit(productions, unit_rules_dict):
         for unit_var in unit_rules_dict[variable]:
             productions[variable].update(productions[unit_var])
             productions[variable].remove(tuple(unit_var))
+
+
+
+def binarize(formalized_grammar):
+    products = formalized_grammar['productions']
+    variables = formalized_grammar['variables']
+    longs = _find_long_RHS(products)
+
+    while(len(longs) != 0):
+        _binarize_help(products, variables, longs)
+        longs = _find_long_RHS(products)
+
+def _find_long_RHS(productions):
+    long_RHS_dict = dict()
+    for variable in productions:
+        for rule in productions[variable]:
+            if len(rule) > 2:
+                if variable not in long_RHS_dict:
+                    long_RHS_dict[variable] = set()
+                long_RHS_dict[variable].add(rule)
+    return long_RHS_dict
+
+def _binarize_help(productions, variables, long_rules):
+    for variable in long_rules:
+        for rule in long_rules[variable]:
+            new_var = _generate_new_variable(productions, variables, rule[1:])
+            productions[variable].add((rule[0], new_var))
+            productions[variable].remove(rule)
