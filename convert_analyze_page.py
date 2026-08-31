d3534132e9e38c7e6986f9b99a72d3188ee0c7a4
KamranMahdavi <Kamranz.mahdaviz@gmail.com>
2026-08-31 01:24:53 -0300

    Implement initial convert and analyze tab layout

---
+++ convert_analyze_page.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import (
+
+    QApplication,
+    QWidget,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout
+
+)
+
+from PyQt5.QtCore import Qt
+
+class ConvertAnalyzeTab(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.subtitle1 = QLabel(
+            "Understand Chomsky Normal Form\nstep by step."
+        )
+        self.subtitle1.setAlignment(Qt.AlignCenter)
+        self.subtitle2 = QLabel(
+            "Convert a context-free grammar "
+            "directly to CNF, or see\nhow each "
+            "conversion step changes it."
+        )
+        self.subtitle2.setAlignment(Qt.AlignCenter)
+        self.convert = QPushButton("Convert Grammar")
+        self.analyze = QPushButton("Analyze Grammar")
+
+        self.setup_layout()
+
+    def setup_layout(self):
+        main_layout = QVBoxLayout()
+        buttons_layout = QHBoxLayout()
+
+        buttons_layout.addWidget(self.convert)
+        buttons_layout.addWidget(self.analyze)
+
+        main_layout.addWidget(self.subtitle1, alignment=Qt.AlignCenter)
+        main_layout.addWidget(self.subtitle2, alignment=Qt.AlignCenter)
+        main_layout.addLayout(buttons_layout)
+
+        self.setLayout(main_layout)
