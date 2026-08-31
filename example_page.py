bb521ec3873794f3f4b03a3bf357994ebb4ebbfc
KamranMahdavi <Kamranz.mahdaviz@gmail.com>
2026-08-31 02:10:55 -0300

    Implement initial example tab layout

---
+++ example_page.py
@@ -0,0 +1,42 @@
+from PyQt5.QtWidgets import (
+
+    QApplication,
+    QWidget,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout
+
+)
+
+from PyQt5.QtCore import Qt
+
+class ExampleTab(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.subtitle1 = QLabel("Learn with an example:")
+        self.grammar_example_label = QLabel(
+            "S → ASB | aC\n"
+            "A → ε | B\n"
+            "B → b\n"
+            "C → c\n"
+        )
+        self.subtitle2 = QLabel(
+            "See how different steps like ε-removal\n"
+            "or binarization transform this grammar."
+        )
+        self.subtitle2.setAlignment(Qt.AlignCenter)
+        self.example_button = QPushButton("Explore Example")
+
+        self.setup_layout()
+
+    def setup_layout(self):
+        main_layout = QVBoxLayout()
+
+        main_layout.addWidget(self.subtitle1, alignment=Qt.AlignCenter)
+        main_layout.addWidget(self.grammar_example_label, alignment=Qt.AlignCenter)
+        main_layout.addWidget(self.subtitle2, alignment=Qt.AlignCenter)
+        main_layout.addWidget(self.example_button)
+
+        self.setLayout(main_layout)
