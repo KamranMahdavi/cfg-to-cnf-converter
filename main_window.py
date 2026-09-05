a35ad1ece805aeb75d0d5bbb447a819234ea58ca
KamranMahdavi <Kamranz.mahdaviz@gmail.com>
2026-09-05 01:56:15 -0300

    Add theme selection controls

--- main_window.py
+++ main_window.py
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import (
     QLabel
 )
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPropertyAnimation
 from convert_analyze_page import ConvertAnalyzeTab
 from example_page import ExampleTab
 
@@ -71,18 +71,38 @@ class OptionsSideBar(QFrame):
     def __init__(self):
         super().__init__()
         self.themes_button = QPushButton("Themes")
+        self.light_button = QPushButton("Light")
+        self.dark_button = QPushButton("Dark")
+        self.themes_frame = QWidget()
         self.about_button = QPushButton("About")
         self.help_button = QPushButton("Help")
 
+        self.setup_connections()
         self.setup_layout()
         self.setFrameShape(QFrame.StyledPanel)
         self.setFrameShadow(QFrame.Raised)
 
+    def setup_connections(self):
+        self.themes_button.clicked.connect(self.show_theme_options)
+
     def setup_layout(self):
+        themes_layout = QVBoxLayout()
+        themes_layout.addWidget(self.light_button)
+        themes_layout.addWidget(self.dark_button)
+        self.themes_frame.setLayout(themes_layout)
+
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.themes_button)
+        main_layout.addWidget(self.themes_frame)
+        self.themes_frame.hide()
         main_layout.addWidget(self.about_button)
         main_layout.addWidget(self.help_button)
         main_layout.addStretch()
 
         self.setLayout(main_layout)
+
+    def show_theme_options(self):
+        if self.themes_frame.isVisible():
+            self.themes_frame.hide()
+        else:
+            self.themes_frame.show()
