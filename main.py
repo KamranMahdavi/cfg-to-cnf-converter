b12ac9c9cf866fc7b51571cb75a9189de6cc4ab9
KamranMahdavi <Kamranz.mahdaviz@gmail.com>
2026-09-06 02:29:13 -0300

    Implement main file and add application icon

---
+++ main.py
@@ -0,0 +1,9 @@
+from main_window import MainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+
+app = QApplication([])
+app.setWindowIcon(QIcon("assets/icon.ico"))
+window = MainWindow()
+window.show()
+app.exec_()
